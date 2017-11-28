1c7f2ecc6eec070460a4fdf0d2c5d23c8e11d023
yuanjie92 <425697230@qq.com>
2017-11-28 17:01:07 +0800

    简单的登录功能

--- src/com/training/controller/LoginController.java
+++ src/com/training/controller/LoginController.java
@@ -1,17 +1,47 @@
 package com.training.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.training.data.UserData;
+import com.training.form.UserForm;
 import com.training.service.UserService;
 
 @Controller
 public class LoginController {
 
+	@Resource
 	private UserService userService;
+
+	// Validator Spring框架里的一个接口
+	@Resource
+	private Validator validator;
 
 	@RequestMapping("/login")
 	public String login() {
 		return "user/login";
+	}
+
+	public String login(UserForm userForm, BindingResult bindingResult, HttpSession session) {
+
+		validator.validate(userForm, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			return "user/login";
+		}
+
+		UserData userData = userService.findUser(userForm);
+		if (null == userData) {
+			return "user/login";
+		}
+
+		session.setAttribute("userData", userData);
+
+		return "student/loadStudentByFields";
 	}
 }
--- src/com/training/service/impl/UserServiceImpl.java
+++ src/com/training/service/impl/UserServiceImpl.java
@@ -1,13 +1,48 @@
 package com.training.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+
 import com.training.common.service.CommonService;
 import com.training.dao.UserDao;
+import com.training.data.UserData;
+import com.training.form.UserForm;
+import com.training.model.UserModel;
 import com.training.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
 	private UserDao userDao;
 	private CommonService commonService;
+	private Md5PasswordEncoder md5Encoder;
+
+	@Override
+	public UserData findUser(UserForm userForm) {
+		UserData userData = new UserData();
+		Map<String, Object> fields = new HashMap<String, Object>();
+		fields.put(UserModel.NAME, userForm.getName());
+		String password = md5Encoder.encodePassword(userForm.getPassword(), userForm.getName());
+		fields.put(UserModel.PASSWORD, password);
+		List<UserModel> userModels = commonService.getEntitiesByFields(UserModel.class, fields);
+		List<UserData> userDatas = new ArrayList<UserData>();
+		if (null != userModels) {
+			for (UserModel model : userModels) {
+				UserData data = new UserData();
+				data.setId(model.getId());
+				data.setName(model.getName());
+				data.setMobile(model.getMobile());
+				data.setCreateDate(model.getCreateDate());
+				data.setUpdateDate(model.getUpdateDate());
+				userDatas.add(data);
+			}
+			userData = userDatas.get(0);
+		}
+		return userData;
+	}
 
 	public UserDao getUserDao() {
 		return userDao;
@@ -23,6 +58,14 @@ public class UserServiceImpl implements UserService {
 
 	public void setCommonService(CommonService commonService) {
 		this.commonService = commonService;
+	}
+
+	public Md5PasswordEncoder getMd5Encoder() {
+		return md5Encoder;
+	}
+
+	public void setMd5Encoder(Md5PasswordEncoder md5Encoder) {
+		this.md5Encoder = md5Encoder;
 	}
 
 }
